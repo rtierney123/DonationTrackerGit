e3b932c9f4c8b8d6a06b299c902df8a38673ce06
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-09-17 21:33:17 -0400

    Added Firebase library to gradle and package folders for controller and model

--- app/src/main/java/com/track/brachio/donationtracker/controller/MainActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.track.brachio.donationtracker;
+package com.track.brachio.donationtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.track.brachio.donationtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/track/brachio/donationtracker/model/User.java
@@ -0,0 +1,4 @@
+package com.track.brachio.donationtracker.model;
+
+public class User {
+}
