775270fe49063ab13d928fba2ae3c7e7410fd6d0
Lauren Vossler <laurencvossler@gmail.com>
2018-10-01 17:00:32 -0400

    Logout Buttons galore
    should have logout buttons on all mains

--- app/src/main/java/com/track/brachio/donationtracker/DonatorMainActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/DonatorMainActivity.java
@@ -1,14 +1,29 @@
 package com.track.brachio.donationtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.track.brachio.donationtracker.model.database.FirebaseUserHandler;
 
 public class DonatorMainActivity extends Activity {
 
+    private Button donMainLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_donator_main );
+        donMainLogout = (Button) findViewById(R.id.logoutButton1);
+        donMainLogout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FirebaseUserHandler handler = new FirebaseUserHandler();
+                handler.signOutUser();
+                Intent intent = new Intent(VolunteerMainActivity.this, StartUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
