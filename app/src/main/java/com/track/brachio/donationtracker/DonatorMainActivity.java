cc699f44206a5ac8e60c573c2548b5344242a3ca
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-10-01 16:44:21 -0400

    Added admin, donator, and manager activity

---
+++ app/src/main/java/com/track/brachio/donationtracker/DonatorMainActivity.java
@@ -0,0 +1,14 @@
+package com.track.brachio.donationtracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DonatorMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_donator_main );
+    }
+
+}
