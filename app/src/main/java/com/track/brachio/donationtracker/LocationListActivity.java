972155a63652f633b7a737745916bfeaaa2b2bbc
Jessica Bishop <jbishop49@gatech.edu>
2018-10-10 12:46:21 -0400

    Add LocationActivity and LocationListActivity

---
+++ app/src/main/java/com/track/brachio/donationtracker/LocationListActivity.java
@@ -0,0 +1,19 @@
+package com.track.brachio.donationtracker;import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.track.brachio.donationtracker.model.User;
+import com.track.brachio.donationtracker.model.UserType;
+import com.track.brachio.donationtracker.model.database.FirebaseUserHandler;
+import com.track.brachio.donationtracker.model.singleton.CurrentUser;
+import com.track.brachio.donationtracker.model.LocationType;
+
+
+public class LocationListActivity {
+
+}
