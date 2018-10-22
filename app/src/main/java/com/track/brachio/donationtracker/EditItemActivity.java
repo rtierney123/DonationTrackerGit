310e9d08081c0c3ac6baec8a46f51ab7271f7f62
Jessica Bishop <jbishop49@gatech.edu>
2018-10-22 17:55:09 -0400

    added Recycler View

--- app/src/main/java/com/track/brachio/donationtracker/EditItemActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/EditItemActivity.java
@@ -23,8 +23,13 @@ import com.track.brachio.donationtracker.model.singleton.CurrentUser;
 import com.track.brachio.donationtracker.model.singleton.CurrentItem;
 import com.track.brachio.donationtracker.model.LocationType;
 import com.track.brachio.donationtracker.model.Location;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
 import com.track.brachio.donationtracker.model.Item;
 import com.track.brachio.donationtracker.model.ItemType;
+import android.view.ViewGroup;
+import java.util.ArrayList;
 
 public class EditItemActivity extends AppCompatActivity {
     private TextView itemName;
@@ -34,11 +39,15 @@ public class EditItemActivity extends AppCompatActivity {
     private EditText newLongDescription;
     private EditText newDollarValue;
     private Spinner newItemCategory;
+    private RecyclerView newCommentsRecyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
     private EditText newComments;
     private ImageView newimage;
     private Button cancelButton;
     private Button addButton;
     private Button deleteButton;
+    private ArrayList<String> comments;
 
     Item currentItem;
 
@@ -54,11 +63,18 @@ public class EditItemActivity extends AppCompatActivity {
         newLongDescription = (EditText) findViewById(R.id.editItemLongDescriptionID);
         newDollarValue = (EditText) findViewById(R.id.editItemValueID);
         newItemCategory = (Spinner) findViewById(R.id.editItemCategoryID);
-        newComments = (EditText) findViewById(R.id.editItemCommentsID);
+        newComments = (EditText) findViewById(R.id.editItemAddCommentID);
+        newCommentsRecyclerView = (RecyclerView) findViewById(R.id.editItemCommentsID);
         newimage = (ImageView) findViewById(R.id.editItemImageID);
         cancelButton = (Button) findViewById(R.id.editItemCancelButton);
         addButton = (Button) findViewById(R.id.editItemMakeChangesID);
         deleteButton = (Button) findViewById(R.id.editItemDeleteButton);
+
+        //DO WE NEED??
+        newCommentsRecyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        newCommentsRecyclerView.setLayoutManager(layoutManager);
 
 
         FirebaseLocationHandler handler = new FirebaseLocationHandler();
@@ -81,7 +97,6 @@ public class EditItemActivity extends AppCompatActivity {
             if (currentItem.getCategory() != null) {
                 newItemCategory.setSelection(Item.findItemTypePosition(currentItem.getCategory()));
             }
-            //set default for item category
             //set default for comments
             //newComments.setText(currentItem.getComments());
             //set default for image
@@ -98,8 +113,6 @@ public class EditItemActivity extends AppCompatActivity {
                 String commentEntered = newComments.getText().toString();
                 ItemType itemTypeSelected = (ItemType) newItemCategory.getSelectedItem();
 
-
-                //spinner
                 //image
                 if (!locationEntered.isEmpty() && !locationEntered.equals(currentItem.getLocation())) {
                     currentItem.setLocation(locationEntered);
@@ -156,5 +169,70 @@ public class EditItemActivity extends AppCompatActivity {
 
          });
 
+    }
+
+    public void setCommentsARray(ArrayList<String> array) {
+        comments = array;
+    }
+
+    public void setUpRecyclerView(RecyclerView view) {
+
+    }
+
+    public void populateRecycleView(ArrayList<String> comm) {
+        comments = comm;
+
+        if (comments != null) {
+            adapter = new EditItemActivity.CommentListAdapter(comments);
+            newCommentsRecyclerView.setAdapter(adapter);
+        } else {
+            Log.d("Comments", "Comments are null");
+        }
+    }
+
+    private static class CommentListAdapter extends RecyclerView.Adapter<EditItemActivity.CommentListAdapter.CommentViewHolder> {
+        private ArrayList<String> theComments;
+        private View view;
+
+        public static class CommentViewHolder extends RecyclerView.ViewHolder {
+            private TextView commentText;
+            private View v;
+            public CommentViewHolder(View v) {
+                super(v);
+                this.v = v;
+                commentText = (TextView) v.findViewById(R.id.textView14);
+            }
+
+            //do I need Bind?
+
+        }
+
+        public CommentListAdapter(ArrayList<String> array) {
+            this.theComments = array;
+        }
+
+        @Override
+        public EditItemActivity.CommentListAdapter.CommentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_comments_list, parent, false);
+            view = v;
+            EditItemActivity.CommentListAdapter.CommentViewHolder vh = new EditItemActivity.CommentListAdapter.CommentViewHolder(v);
+            return vh;
+        }
+
+        //idk if this is right
+        @Override
+        public void onBindViewHolder(EditItemActivity.CommentListAdapter.CommentViewHolder holder, int position) {
+            holder.commentText.setText(theComments.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return theComments.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
     }
 }
