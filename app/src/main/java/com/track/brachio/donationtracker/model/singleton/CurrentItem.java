2091e7ca9d2ffcdd896bc23ee1cb20101b998c54
Jessica Bishop <jbishop49@gatech.edu>
2018-11-05 21:44:53 -0500

    I love javadocs

--- app/src/main/java/com/track/brachio/donationtracker/model/singleton/CurrentItem.java
+++ app/src/main/java/com/track/brachio/donationtracker/model/singleton/CurrentItem.java
@@ -2,6 +2,9 @@ package com.track.brachio.donationtracker.model.singleton;
 
 import com.track.brachio.donationtracker.model.Item;
 
+/**
+ * Information holder for CurrentItem
+ */
 public final class CurrentItem {
 
     // static variable single_instance of type CurrentUser
@@ -11,12 +14,21 @@ public final class CurrentItem {
     private Item item;
 
     // private constructor restricted to this class itself
+
+    /**
+     * Constructor for CurrentItem
+     */
     private CurrentItem()
     {
         item = new Item();
     }
 
     // static method to create instance of CurrentUser class
+
+    /**
+     * returns currentInstance being used
+     * @return current instance of Item
+     */
     public static CurrentItem getInstance()
     {
         if (single_instance == null) {
@@ -26,10 +38,18 @@ public final class CurrentItem {
         return single_instance;
     }
 
+    /**
+     * setter for Item
+     * @param i item being set to
+     */
     public void setItem(Item i) {
         item = i;
     }
 
+    /**
+     * getter for Item
+     * @return item being returned
+     */
     public Item getItem(){
         return item;
     }
