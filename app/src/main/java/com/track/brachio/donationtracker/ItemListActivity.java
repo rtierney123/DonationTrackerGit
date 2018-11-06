a3e1597800646aa18af719cd9890b6b0fc5bc786
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-11-06 01:57:46 -0500

    Added toolbar to LoginActivity

--- app/src/main/java/com/track/brachio/donationtracker/ItemListActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/ItemListActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -132,6 +134,8 @@ public class ItemListActivity extends AppCompatActivity{
             }
         });
     }
+
+
 
     @Override
     protected void onResume(){
--- app/src/main/java/com/track/brachio/donationtracker/LoginActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/LoginActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.track.brachio.donationtracker.controller.PersistanceManager;
 import com.track.brachio.donationtracker.model.User;
@@ -19,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button cancelButton;
     private EditText usernameField;
     private EditText passwordField;
+    private ImageButton optionButton;
 
     //user logging in if successful
     private User _currentUser;
@@ -63,7 +67,10 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+
     }
+
+
 
     /**
      * goes to the correct location for user type
