85e85feff57848ecf1c36697a45dcf7006b8c52b
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-11-12 20:53:45 -0500

    Added stuff for mockito and unit test.

---
+++ app/src/androidTest/java/com/track/brachio/donationtracker/model/RachelTest.java
@@ -0,0 +1,14 @@
+package com.track.brachio.donationtracker.model;
+
+import org.junit.Test;
+import java.util.List;
+import java.util.Arrays;
+
+//Tests for addItem method in FirebaseItemHandler
+public class RachelTest {
+
+    @Test
+    public void testThingy(){
+
+    }
+}
