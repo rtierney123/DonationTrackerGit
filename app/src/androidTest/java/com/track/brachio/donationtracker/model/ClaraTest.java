5e01f61515442be4955bfa7b20fdcfdff08416ab
ckennedy46 <41365122+ckennedy46@users.noreply.github.com>
2018-11-13 13:30:01 -0500

    junit tests for Clara

---
+++ app/src/androidTest/java/com/track/brachio/donationtracker/model/ClaraTest.java
@@ -0,0 +1,49 @@
+package com.track.brachio.donationtracker.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Clara Junit Tests
+ */
+public class ClaraTest {
+//    /**
+//    * Tests if input is empty string
+//    */
+//    @Test
+//    public void testNullInput() {
+//        assertEquals("Empty string should return null", null, Item.stringToItemType(""));
+//    }
+//    /**
+//    * Tests if input is not one of the strings
+//    */
+//    @Test
+//    public void testInvalidInput() {
+//        assertEquals("Invalid string should return null", null, Item.stringToItemType("clara"));
+//    }
+//
+//    @Test
+//    public void testFoodInput() {
+//        assertEquals("Food string should return food item type", ItemType.Food, Item.stringToItemType("Food"));
+//    }
+//    @Test
+//    public void testClothesInput() {
+//        assertEquals("Clothes string should return clothes item type", ItemType.Clothes, Item.stringToItemType("Clothes"));
+//    }
+//    @Test
+//    public void testSportInput() {
+//
+//    }
+//    @Test
+//    public void testElectronicInput() {
+//
+//    }
+//    @Test
+//    public void testMiscInput() {
+//
+//    }
+//    @Test
+//    public void testFurnitureInput() {
+//
+//    }
+}
