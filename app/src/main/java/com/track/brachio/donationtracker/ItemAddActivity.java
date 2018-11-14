0d774946a96a3a9c06b56538dfc55b63d6ce5ede
Jessica Bishop <jbishop49@gatech.edu>
2018-11-14 15:47:01 -0500

    fixing imports

--- app/src/main/java/com/track/brachio/donationtracker/ItemAddActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/ItemAddActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -122,4 +122,5 @@ public class ItemAddActivity extends AppCompatActivity {
     }
 
 
-}
+    public void takePicture(View view) {
+    }}
