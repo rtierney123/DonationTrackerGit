7ac481cb284652bb7f3a5ece541e38790f0580f5
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-12-06 12:00:47 -0500

    Got item pics to be rotated correctly.

--- app/src/main/java/com/track/brachio/donationtracker/ItemAddActivity.java
+++ app/src/main/java/com/track/brachio/donationtracker/ItemAddActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.StrictMode;
@@ -44,6 +46,7 @@ public class ItemAddActivity extends AppCompatActivity {
     private ImageButton newImage;
     private final static int THUMBNAIL_SIZE = 200;
     private Uri file;
+    private Bitmap bitmap;
 
     private final Activity currentActivity = this;
     //Item currentItem;
@@ -117,12 +120,8 @@ public class ItemAddActivity extends AppCompatActivity {
                 if (!commentEntered.isEmpty()) {
                     newItem.addComment(commentEntered);
                 }
-                if (file != null){
-                    try {
-                        newItem.bitmap = getThumbnail( file );
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                if (bitmap != null){
+                    newItem.bitmap = bitmap;
                 }
 
                 manager.addItem(newItem, currentActivity);
@@ -150,10 +149,18 @@ public class ItemAddActivity extends AppCompatActivity {
         if (requestCode == 100) {
             if (resultCode == RESULT_OK) {
                 newImage.setImageURI(file);
-                Bitmap bitmap = null;
+                ExifInterface exif = null;
+                try {
+                    exif = new ExifInterface(file.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+               bitmap = null;
                 try {
                     bitmap = getThumbnail( file );
-                    //bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    bitmap = rotateBitmap(bitmap, orientation);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -161,6 +168,50 @@ public class ItemAddActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        }
+        catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /**
